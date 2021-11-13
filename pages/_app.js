5fc412505e3266fc49d4b5b5252bb1ebc088c892
Caleb Nelson <caleb.nelson@faithlife.com>
2021-11-13 09:19:03 -0800

    Added some global styles and theme values.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,37 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import "./_app.css"
+
+const theme = {
+  colors: {
+    primary: '#b8b8b8',
+		secondary: '#2c2c2c'
+  },
+	fonts: {
+		primary: '"Roboto", sans-serif',
+		secondary: '"Rokkitt", serif;'
+	},
+	breakpoints: {
+		xxl: '1899px',
+		xl: '1599px',
+		lg: '1279px',
+		md: '991px',
+		sm: '767px',
+		xs: '575px',
+		xxs: '374px'
+	},
+	space: {
+    zero: "0px",
+    xs: "2px",
+    sm: "4px",
+    ms: "8px",
+    md: "12px",
+    ml: "16px",
+    lg: "24px",
+    xl: "32px",
+    xxl: "64px",
+    xxxl: "96px"
+	}
+}
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -6,13 +39,53 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
-`
 
-const theme = {
-  colors: {
-    primary: '#b8b8b8',
-  },
-}
+	h1,h2,h3,h4,h5,h6 {
+		font-weight: 900;
+	}
+
+	h1,h2,h3 {
+		font-family: ${(theme.fonts.primary)}
+	}
+
+	h4,h5,h6,p{
+		font-family: ${(theme.fonts.secondary)}
+	}
+
+	h1 {
+		font-size: 52px;
+		line-height: 55px;
+
+		@media (max-width: ${(theme.breakpoints.md)}) {
+			font-size: 36px;
+			line-height: 42px;
+		}
+	}
+
+	h2 {
+		font-size: 30px;
+		line-height: 34px;
+
+		@media (max-width: ${(theme.breakpoints.md)}) {
+			font-size: 28px;
+			line-height: 34px;
+		}
+
+	h3 {
+		font-size: 22px;
+		line-height: 28px;
+	}
+
+	h4 {
+		font-size: 18px;
+		line-height: 24px;
+	}
+
+	h5 {
+		font-size: 16px;
+		line-height: 20px;
+	}
+`
 
 export default function App({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-import ComingSoon from '../hoc/ComingSoon';
+import ComingSoon from '../components/styles/ComingSoon.styled';
 import styled from 'styled-components'
 
 const Title = styled.h1`
-  font-size: 50px;
 	text-align: center;
   color: ${({ theme }) => theme.colors.primary};
 `

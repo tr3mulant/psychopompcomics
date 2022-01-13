d19ba4f4c238561e1850559894fe84437ff66291
Caleb Nelson <caleb.nelson@faithlife.com>
2022-01-13 10:29:18 -0800

    Added favicon to Head on each page.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import ComingSoon from '../components/styles/ComingSoon.styled'
 import styled from 'styled-components'
 import ComingSoonImage from '../components/ComingSoonImage';
-import deer from '../images/deer.svg'; 
 import NewsletterSubscribe from '../components/NewsletterSubscribe';
 
 const Title = styled.h1`
@@ -10,10 +9,12 @@ const Title = styled.h1`
 	margin: 0 0 32px;
 `
 export default function Home() {
-  return <ComingSoon>
-		<Title>Coming Soon</Title>
-		<ComingSoonImage />
-		<h2>Sign up to receive email updates</h2>
-		<NewsletterSubscribe />
-	</ComingSoon>
+  return <>
+		<ComingSoon>
+			<Title>Coming Soon</Title>
+			<ComingSoonImage />
+			<h2>Sign up to receive email updates</h2>
+			<NewsletterSubscribe />
+		</ComingSoon>
+	</>
 }

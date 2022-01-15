dabdc68b2aa5473b73fb8028d84795948615f8a4
Caleb Nelson <caleb.nelson@faithlife.com>
2022-01-14 19:09:26 -0800

    Formatted files with Prettier.

--- ReadMe.md
+++ ReadMe.md
@@ -1,8 +1,11 @@
 # Installation
-* [Install Docker Engine](https://docs.docker.com/engine/install/)
-* [Install Docker Compose](https://docs.docker.com/compose/install/)
-* `yarn install`
+
+- [Install Docker Engine](https://docs.docker.com/engine/install/)
+- [Install Docker Compose](https://docs.docker.com/compose/install/)
+- `yarn install`
 
 # Run
+
 Build and run images (nextjs hot reload proxied by nginx)
-* `yarn doc-dev`
+
+- `yarn doc-dev`
--- components/styles/EmailCaptureContainer.styled.js
+++ components/styles/EmailCaptureContainer.styled.js
@@ -1,29 +1,29 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const EmailCaptureContainer = styled.article`
-	max-width: 334px;
-	margin: 24px auto;
-	display: flex;
-	justify-content: center;
-	align-items: stretch;
-	box-shadow: var(--box-shadow-xxs);
-	border-top-right-radius: 4px;
-	border-bottom-right-radius: 4px;
+  max-width: 334px;
+  margin: 24px auto;
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+  box-shadow: var(--box-shadow-xxs);
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px;
 
-	& input {
-		max-width: calc(100% - 109px);
-		border-top: 1px solid var(--brand);
-		border-left: 1px solid var(--brand);
-		border-bottom: 1px solid var(--brand);
-		padding-left: 4px;
-		background-color: var(--surface2);
-		color: var(--text2);
-	}
+  & input {
+    max-width: calc(100% - 109px);
+    border-top: 1px solid var(--brand);
+    border-left: 1px solid var(--brand);
+    border-bottom: 1px solid var(--brand);
+    padding-left: 4px;
+    background-color: var(--surface2);
+    color: var(--text2);
+  }
 
-	& button {
-		border-top-left-radius: 0;
-		border-bottom-left-radius: 0;
-	}
+  & button {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+  }
 `;
 
 export default EmailCaptureContainer;

8c5827db33c62f6d886a46a0d857ada20abfdc20
Caleb Nelson <caleb.nelson@faithlife.com>
2022-03-05 09:33:09 -0800

    Added motion elements for navigation transitions and to respect reduced motion for accessability.

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,26 @@ import { theme } from '../components/themes/DefaultTheme';
 import { GlobalStyle } from '../components/styles/GlobalStyles.styled';
 import HeadTag from '../components/HeadTag';
 import { Navbar } from '../components/Navbar';
+import { MotionConfig, AnimatePresence } from 'framer-motion';
 import './_app.css';
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps, router }) {
+	const url = `${
+		process.env.NODE_ENV == 'production'
+			? 'https://psychopompcomics.com'
+			: 'localhost'
+	}${router.route}`;
 	return (
 		<>
 			<HeadTag />
 			<GlobalStyle />
 			<ThemeProvider theme={theme}>
-				{ process.env.NODE_ENV !== "production" ? <Navbar /> : null}
-				<Component {...pageProps} />
+				<MotionConfig reducedMotion='user'>
+					{process.env.NODE_ENV !== 'production' ? <Navbar /> : null}
+					<AnimatePresence exitBeforeEnter>
+						<Component {...pageProps} canonical={url} key={url} />
+					</AnimatePresence>
+				</MotionConfig>
 			</ThemeProvider>
 		</>
 	);

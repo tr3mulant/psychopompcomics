9dd0141166c00e1ab19aaa8531bf678fba7bef6d
Caleb Nelson <caleb.nelson@faithlife.com>
2022-03-15 18:10:27 -0700

    Added role='banner' to motion header.

--- components/styles/MotionHeader.styled.js
+++ components/styles/MotionHeader.styled.js
@@ -10,6 +10,7 @@ export default function MotionHeader(props) {
 	const theme = useTheme();
 	return (
 		<StyledHeader
+			role='banner'
 			variants={theme.motion.pageTransitionVariants}
 			initial='hidden'
 			animate='visible'

52402bab38a05f67e2534043c1e6f48820e24d64
Caleb Nelson <caleb.nelson@faithlife.com>
2022-03-17 22:56:16 -0700

    More tweaking to prevent elements from overflowing the page resulting in x-axis scrolling. The lynch pin was setting overflow: hidden on Main.

--- components/styles/StyledSection.styled.js
+++ components/styles/StyledSection.styled.js
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 const StyledSection = styled.section`
 	position: relative;
 	max-width: 100%;
-	padding-left: ${(props) => props.theme.spaces.md};
-	padding-right: ${(props) => props.theme.spaces.md};
+	padding-left: ${(props) => props.theme.spaces.ml};
+	padding-right: ${(props) => props.theme.spaces.ml};
+	overflow: hidden;
 `;
 
 export default StyledSection;

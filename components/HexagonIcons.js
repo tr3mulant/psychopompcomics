e7589a3c1ff05b49180869ffd208195868f979bf
Caleb Nelson <caleb.nelson@faithlife.com>
2022-03-30 18:32:37 -0700

    Updated components to be able to take Image properties if provided, otherwise uses default values.

--- components/HexagonIcons.js
+++ components/HexagonIcons.js
@@ -7,41 +7,71 @@ import blockchain_dark from '../images/icons/hexagon-blockchain-dark.svg';
 import microchip_light from '../images/icons/hexagon-microchip-light.svg';
 import microchip_dark from '../images/icons/hexagon-microchip-dark.svg';
 
-export function RobotIconHexagon() {
+export function RobotIconHexagon(props) {
 	const theme = useTheme();
 	const logo = theme.mode == 'light' ? robot_light : robot_dark;
+	const {
+		width = 1080,
+		height = 935.31,
+		src = logo,
+		alt = 'Robot in a hexagaon with a Heart emoji face',
+		layout = 'responsive',
+		...other
+	} = props;
 	return (
 		<Image
-			width='1080'
-			height='935.31'
 			src={logo}
-			alt='Robot in a hexagaon with a Heart emoji face'
+			alt={alt}
+			width={width}
+			height={height}
+			layout={layout}
+			{...other}
 		/>
 	);
 }
 
-export function BlockchainIconHexagon() {
+export function BlockchainIconHexagon(props) {
 	const theme = useTheme();
 	const logo = theme.mode == 'light' ? blockchain_light : blockchain_dark;
+	const {
+		width = 1080,
+		height = 935.31,
+		src = logo,
+		alt = 'Blockchain in a hexagon',
+		layout = 'responsive',
+		...other
+	} = props;
 	return (
 		<Image
-			width='1080'
-			height='935.31'
 			src={logo}
-			alt='Blockchain in a hexagon'
+			alt={alt}
+			width={width}
+			height={height}
+			layout={layout}
+			{...other}
 		/>
 	);
 }
 
-export function MicrochipIconHexagon() {
+export function MicrochipIconHexagon(props) {
 	const theme = useTheme();
 	const logo = theme.mode == 'light' ? microchip_light : microchip_dark;
+	const {
+		width = 1080,
+		height = 935.31,
+		src = logo,
+		alt = 'Microchip in a hexagon',
+		layout = 'responsive',
+		...other
+	} = props;
 	return (
 		<Image
-			width='1080'
-			height='935.31'
 			src={logo}
-			alt='Microchip in a hexagon'
+			alt={alt}
+			width={width}
+			height={height}
+			layout={layout}
+			{...other}
 		/>
 	);
 }

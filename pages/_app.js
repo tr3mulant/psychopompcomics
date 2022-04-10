60970242c83ca0fbba2ff9c2c486e0cd9157e598
Caleb Nelson <caleb.nelson@faithlife.com>
2022-04-09 23:16:41 -0700

    Framer motion now using Lazy motion and forcing use of m instead of motion.

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { themeDark, themeLight } from '../components/themes/DefaultTheme';
 import { GlobalStyle } from '../components/styles/GlobalStyles.styled';
 import HeadTag from '../components/HeadTag';
 import TagManager from 'react-gtm-module/dist/TagManager';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { MenuProvider } from '../state/useMenuContext';
 import { MotionNavbar } from '../components/Navbar';
 import {
@@ -13,7 +13,6 @@ import {
 	domAnimation,
 } from 'framer-motion';
 import './_app.css';
-import { useState } from 'react';
 
 export default function App({ Component, pageProps, router }) {
 	const [theme, setTheme] = useState('dark');
